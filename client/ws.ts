c82b96d87c29a2148074e0d0a9c558c0c9a1d763
Vladyslav Hryhola <v.hryhola@astoundcommerce.com>
2023-01-05 22:19:54 +0200

    updated ssh

--- client/ws.ts
+++ client/ws.ts
@@ -1,10 +1,12 @@
 type WebSocketCallbacks = { onClose: () => void; onOpen: (ws: WebSocket) => void; onError: () => void }
 
-export const initUWS = (siteLink: string) => fetch(`http://${siteLink}/api/init`)
+export const initUWS = (siteLink: string) => fetch(`${process.env.NODE_ENV === 'production' ? 'https' : 'http'}://${siteLink}/api/init`)
 
 let isHandlingConnectRequest = false
 
 export const connectToWebSocket = async (hostname: string, port: string, callbacks?: WebSocketCallbacks) => {
+    console.log(process.env.NODE_ENV)
+
     if (isHandlingConnectRequest) {
         console.log('Already handling connecting request. Exiting.')
 
@@ -18,7 +20,7 @@ export const connectToWebSocket = async (hostname: string, port: string, callbac
     return initUWS(siteLink)
         .catch(e => console.log('UWS Init error', e))
         .finally(() => {
-            const url = `ws://${hostname}:5555`
+            const url = `${process.env.NODE_ENV === 'production' ? 'wss' : 'ws'}://${hostname}:5555`
 
             const ws = new WebSocket(url)
 
