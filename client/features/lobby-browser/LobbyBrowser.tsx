40479507d62247b8896a00b36f69f000930bd110
Vladyslav Hryhola <v.hryhola@astoundcommerce.com>
2023-01-08 17:00:56 +0200

    added lobbies filtering

--- client/features/lobby-browser/LobbyBrowser.tsx
+++ client/features/lobby-browser/LobbyBrowser.tsx
@@ -1,53 +1,14 @@
-import { Box, IconButton, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Toolbar } from '@mui/material'
+import { Box, FormControl, IconButton, InputAdornment, List, ListItem, ListItemButton, ListItemIcon, ListItemText, TextField, Toolbar } from '@mui/material'
 import { WSContext } from 'client/context/list/ws.context'
 import { useContext, useEffect, useState } from 'react'
 import { LobbyInfo } from 'uws/api/Lobby-GetList'
 import { RequestData, RequestHandler } from 'uws/uws.types'
 import AddIcon from '@mui/icons-material/Add'
 import { styled, alpha } from '@mui/material/styles'
+import { Input, OutlinedInput } from '@mui/material'
 import InputBase from '@mui/material/InputBase'
 import SearchIcon from '@mui/icons-material/Search'
 import LockIcon from '@mui/icons-material/Lock'
-
-const Search = styled('div')(({ theme }) => ({
-    position: 'relative',
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: alpha(theme.palette.common.white, 0.15),
-    '&:hover': {
-        backgroundColor: alpha(theme.palette.common.white, 0.25)
-    },
-    marginRight: theme.spacing(2),
-    marginLeft: 0,
-    width: '100%'
-    // [theme.breakpoints.up('sm')]: {
-    //     marginLeft: theme.spacing(3),
-    //     width: 'auto',
-    // },
-}))
-
-const SearchIconWrapper = styled('div')(({ theme }) => ({
-    padding: theme.spacing(0, 2),
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center'
-}))
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-    color: 'inherit',
-    '& .MuiInputBase-input': {
-        padding: theme.spacing(1, 1, 1, 0),
-        // vertical padding + font size from searchIcon
-        paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-        transition: theme.transitions.create('width'),
-        width: '100%',
-        [theme.breakpoints.up('md')]: {
-            width: '20ch'
-        }
-    }
-}))
 
 const LobbyRecord: React.FC<LobbyInfo> = props => (
     <ListItem disablePadding>
@@ -61,146 +22,43 @@ const LobbyRecord: React.FC<LobbyInfo> = props => (
 export const LobbyBrowser: React.FC = () => {
     const ws = useContext(WSContext)
 
-    const [lobbiesList, setLobbiesList] = useState<LobbyInfo[]>([])
+    const [lobbiesList, setLobbiesList] = useState<LobbyInfo[]>([
+        {
+            id: 'Room',
+            private: false
+        },
+        {
+            id: 'Room 2',
+            private: true
+        },
+        {
+            id: 'Room 3',
+            private: false
+        },
+        {
+            id: 'Room 4',
+            private: false
+        },
+        {
+            id: 'Room 5',
+            private: false
+        }
+    ])
+
+    const [searchString, setSearchString] = useState('')
 
     const getListHandler: RequestHandler<'Lobby-GetList'> = data => {
-        // setLobbiesList(data.lobbies)
-        setLobbiesList([
-            {
-                id: 'Room',
-                private: false
-            },
-            {
-                id: 'Room 2',
-                private: true
-            },
-            {
-                id: 'Room 3',
-                private: false
-            },
-            {
-                id: 'Room 4',
-                private: false
-            },
-            {
-                id: 'Room 2',
-                private: false
-            },
-            {
-                id: 'Room 3',
-                private: true
-            },
-            {
-                id: 'Room 4',
-                private: false
-            },
-            {
-                id: 'Room 4',
-                private: false
-            },
-            {
-                id: 'Room 4',
-                private: false
-            },
-            {
-                id: 'Room 4',
-                private: false
-            },
-            {
-                id: 'Room 4',
-                private: false
-            },
-            {
-                id: 'Room 4',
-                private: false
-            },
-            {
-                id: 'Room 4',
-                private: false
-            },
-            {
-                id: 'Room 4',
-                private: false
-            },
-            {
-                id: 'Room 4',
-                private: false
-            },
-            {
-                id: 'Room 4',
-                private: false
-            },
-            {
-                id: 'Room 4',
-                private: false
-            },
-            {
-                id: 'Room 4',
-                private: false
-            },
-            {
-                id: 'Room 4',
-                private: false
-            },
-            {
-                id: 'Room 4',
-                private: false
-            },
-            {
-                id: 'Room 4',
-                private: false
-            },
-            {
-                id: 'Room 4',
-                private: false
-            },
-            {
-                id: 'Room 4',
-                private: false
-            },
-            {
-                id: 'Room 4',
-                private: false
-            },
-            {
-                id: 'Room 4',
-                private: false
-            },
-            {
-                id: 'Room 4',
-                private: false
-            },
-            {
-                id: 'Room 4',
-                private: false
-            },
-            {
-                id: 'Room 4',
-                private: false
-            },
-            {
-                id: 'Room 4',
-                private: false
-            },
-            {
-                id: 'Room 4',
-                private: false
-            },
-            {
-                id: 'Room 4',
-                private: false
-            },
-            {
-                id: 'Room 5',
-                private: false
-            }
-        ])
+        setLobbiesList(data.lobbies)
     }
 
     useEffect(() => {
-        ws.on('Lobby-GetList', getListHandler)
-        ws.send('Lobby-GetList')
+        // ws.on('Lobby-GetList', getListHandler)
+        // ws.send('Lobby-GetList')
     }, [])
+
+    const renderedLobbies = searchString.length ? lobbiesList.filter(lobby => lobby.id.toLowerCase().includes(searchString.toLowerCase())) : lobbiesList
+
+    console.log(renderedLobbies)
 
     return (
         <>
@@ -208,14 +66,26 @@ export const LobbyBrowser: React.FC = () => {
                 <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
                     <AddIcon />
                 </IconButton>
-                <Search>
-                    <SearchIconWrapper>
-                        <SearchIcon />
-                    </SearchIconWrapper>
-                    <StyledInputBase placeholder="Search…" inputProps={{ 'aria-label': 'search' }} />
-                </Search>
+                <FormControl fullWidth variant="filled">
+                    <OutlinedInput
+                        size="small"
+                        fullWidth
+                        placeholder="Search..."
+                        value={searchString}
+                        onChange={e => setSearchString(e.target.value)}
+                        endAdornment={
+                            <InputAdornment position="end">
+                                <SearchIcon />
+                            </InputAdornment>
+                        }
+                    />
+                </FormControl>
             </Toolbar>
-            <List>{lobbiesList.map(LobbyRecord)}</List>
+            <List>
+                {renderedLobbies.map(lobby => (
+                    <LobbyRecord key={lobby.id} {...lobby} />
+                ))}
+            </List>
         </>
     )
 }
