b9beea03fe00a5d95ec967957a71ecbeecc3fef1
Vlad Hryhola <vladgrygola@gmail.com>
2023-03-07 21:43:39 +0200

    bg gradient for players header

--- client/features/games/common/PlayersHeader.tsx
+++ client/features/games/common/PlayersHeader.tsx
@@ -9,7 +9,16 @@ interface HeaderProps {
 
 const PlayersHeader: React.FC<HeaderProps> = props => {
     return (
-        <Box sx={{ background: 'rgba(0,0,0, 0.25)', zIndex: 2 }} width="100vw" display="flex" justifyContent="center" position="fixed">
+        <Box
+            sx={{
+                background: 'linear-gradient(0deg, rgb(0 0 0 / 0%) 0%, #000024 100%)',
+                zIndex: 2
+            }}
+            width="100vw"
+            display="flex"
+            justifyContent="center"
+            position="fixed"
+        >
             <Grid container width="auto" wrap="nowrap" overflow="auto">
                 {props.isLoading ? (
                     <Grid item>
