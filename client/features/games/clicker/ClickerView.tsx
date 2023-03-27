61022895884c7fb71007b3b08a89d6980e50a79c
Vladyslav Hryhola <v.hryhola@astoundcommerce.com>
2023-03-27 22:26:00 +0300

    added 'game not started' message for clicker spectator

--- client/features/games/clicker/ClickerView.tsx
+++ client/features/games/clicker/ClickerView.tsx
@@ -1,24 +1,19 @@
 import React from 'react'
-import { Dialog, Button } from '@mui/material'
-import { useLobby, useWS } from 'client/context/list'
 import PlayersHeader from '../common/PlayersHeader'
 import { LobbyControls } from 'client/features/lobby-controls/LobbyControls'
 import { ClickerCanvas } from './ClickerCanvas'
 import { createGame } from '../common/GameFactory'
 import { Clicker } from 'state'
+import { ClickerNoSession } from './ClickerNoSession'
 
 export const [ClickerView, useClicker, useClickerAction, useActionSender] = createGame<Clicker>(() => {
     const game = useClicker()
-    const lobby = useLobby()
-    const ws = useWS()
 
     return (
         <>
             <PlayersHeader members={game.players} isLoading={game.isLoading} />
             <ClickerCanvas />
-            <Dialog open={!game.isSessionStarted} sx={{ zIndex: 1 }} disableEnforceFocus>
-                <Button onClick={() => ws.send('Game-Start', { lobbyId: lobby.lobbyId })}>Start game</Button>
-            </Dialog>
+            <ClickerNoSession />
             <LobbyControls />
         </>
     )
