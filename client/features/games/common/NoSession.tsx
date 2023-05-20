6529636e0b9397e185a8a066133bc7544ba4036a
Vladyslav Hryhola <v.hryhola@astoundcommerce.com>
2023-05-20 21:21:57 +0300

    Jeopardy. Only master can start the game

--- client/features/games/common/NoSession.tsx
+++ client/features/games/common/NoSession.tsx
@@ -1,18 +1,28 @@
 import { Button, Dialog } from '@mui/material'
-import { useLobby, useWS } from 'client/context/list'
+import { useLobby, useUser, useWS } from 'client/context/list'
 import { GameCtxValue } from './GameFactory'
 
 type Props = {
     game: GameCtxValue
+    starter?: 'any' | 'master'
 }
 
-export const NoSession: React.FC<Props> = ({ game }) => {
+export const NoSession: React.FC<Props> = props => {
     const ws = useWS()
+    const user = useUser()
     const { lobbyId, myRole } = useLobby()
 
+    const isMasterView = props.game.players.some(p => p.id === user.id && p.playerIsMaster)
+
+    const starter = props.starter || 'any'
+
     return (
-        <Dialog open={!game.isSessionStarted} sx={{ zIndex: 1 }} disableEnforceFocus>
-            {myRole === 'player' ? <Button onClick={() => ws.send('Game-Start', { lobbyId })}>Start game</Button> : 'No game in progress'}
+        <Dialog open={!props.game.isSessionStarted} sx={{ zIndex: 1 }} disableEnforceFocus>
+            {myRole === 'player' && (starter === 'any' || isMasterView) ? (
+                <Button onClick={() => ws.send('Game-Start', { lobbyId })}>Start game</Button>
+            ) : (
+                'No game in progress'
+            )}
         </Dialog>
     )
 }
